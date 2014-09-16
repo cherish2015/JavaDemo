8543da57f754123acc149862b4ff1fd3eda40520
cherish2015 <cherish1896@126.com>
2014-09-16 23:07:31 +0800

    mina server

---
+++ src/test/net/mina/Constants.java
@@ -0,0 +1,11 @@
+package test.net.mina;
+
+import java.nio.charset.Charset;
+
+public class Constants {
+	
+	public static final Charset charset = Charset.forName("UTF-8");
+	
+	public static final int ServerPort = 1896;
+
+}
--- src/test/net/mina/MinaServerTest.java
+++ src/test/net/mina/MinaServerTest.java
@@ -1,10 +1,22 @@
 package test.net.mina;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+import org.apache.mina.core.session.IdleStatus;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
+import org.apache.mina.filter.logging.LoggingFilter;
+import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import test.ITest;
 
-public class MinaServerTest implements ITest{
+public class MinaServerTest implements ITest {
+	
+	private static Logger log = LoggerFactory.getLogger(MinaServerTest.class);
 
 	public static void main(String[] args) {
 	}
@@ -12,7 +24,23 @@ public class MinaServerTest implements ITest{
 	@Test
 	@Override
 	public void test() {
+		NioSocketAcceptor acceptor = new NioSocketAcceptor(4);
+		acceptor.getFilterChain().addLast("logger", new LoggingFilter());
+		acceptor.getFilterChain().addLast(
+				"codex",
+				new ProtocolCodecFilter(new TextLineCodecFactory(
+						Constants.charset)));
+		acceptor.setHandler(new TimeServerHandler());
+		acceptor.getSessionConfig().setReadBufferSize(1024*2);
+		acceptor.getSessionConfig().setIdleTime(IdleStatus.BOTH_IDLE, 20);
 		
+		try {
+			acceptor.bind(new InetSocketAddress(Constants.ServerPort));
+			log.info("server start");
+			log.info("listen port {}",Constants.ServerPort);
+		} catch (IOException e) {
+			log.error(e.getMessage(), e);
+		}
 	}
 
 }
