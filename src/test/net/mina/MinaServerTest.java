b7ff6ba1e04294d5936a88fde853eca7639276e6
cherish2015 <cherish1896@126.com>
2014-09-16 22:36:13 +0800

    111

---
+++ src/test/net/mina/MinaServerTest.java
@@ -0,0 +1,18 @@
+package test.net.mina;
+
+import org.junit.Test;
+
+import test.ITest;
+
+public class MinaServerTest implements ITest{
+
+	public static void main(String[] args) {
+	}
+
+	@Test
+	@Override
+	public void test() {
+		
+	}
+
+}
