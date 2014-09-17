2aaf38b3f8265acdfc800457f1e4dc454b68ce5d
cherish2015 <cherish1896@126.com>
2014-09-17 23:23:04 +0800

    test properties

---
+++ src/test/util/properties/PropertiesUtil.java
@@ -0,0 +1,90 @@
+package test.util.properties;
+
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+import test.util.codec.BaseCodecUtil;
+
+
+public class PropertiesUtil {
+	
+	public static void storeProperties(Map<String, String> kvmap, String file, boolean append) throws Exception{
+		Map<String, String> kvmapCodec = new HashMap<String, String>();
+		Set<String> keys = kvmap.keySet();
+		Iterator<String> it = keys.iterator();
+		while(it.hasNext()){
+			String key = it.next();
+			String value = kvmap.get(key);
+			kvmapCodec.put(keyEncode(key), valueEncode(value));
+		}
+		Properties props = new Properties();
+		props.putAll(kvmapCodec);
+		FileWriter fw = new FileWriter(file,append);
+		props.store(fw, "");
+	}
+		
+	public static String keyEncode(String content) throws Exception{
+		String key = BaseCodecUtil.encodeB32(content);
+		key = BaseCodecUtil.encodeB32(key);
+		key = BaseCodecUtil.encodeB32(key);
+		return key;
+	}
+		
+	public static String valueEncode(String content) throws Exception{
+		String value = BaseCodecUtil.encodeB64(content);
+		value = BaseCodecUtil.encodeB64(value);
+		value = BaseCodecUtil.encodeB64(value);
+		return value;
+	}
+		
+	public static String valueDecode(String content) throws Exception{
+		String value = BaseCodecUtil.decodeB64(content);
+		value = BaseCodecUtil.decodeB64(value);
+		value = BaseCodecUtil.decodeB64(value);
+		return value;
+	}
+	
+	public static Map<String, String> getProperties(List<String> keys,String fileName) throws Exception{
+		InputStream input = new FileInputStream(fileName);
+		Properties props = new Properties();
+		props.load(input);
+		Map<String, String> kvmap = new HashMap<String, String>();
+		for (int i = 0; i < keys.size(); i++) {
+			String key = keys.get(i);
+			String keyCodec = keyEncode(key);
+			if (props.containsKey(keyCodec)) {
+				String valueCodec = props.getProperty(keyCodec);
+				String value = valueDecode(valueCodec);
+				kvmap.put(key, value);
+			}else{
+				return null;
+			}
+		}
+		return kvmap;
+	}
+	
+	public static void saveCookies(List<String> cookieKeyValues, String propertiesFile, boolean append) throws Exception{
+
+		Map<String, String> mapCookies = new HashMap<String, String>();
+		for (int i = 0; i < cookieKeyValues.size(); i++) {
+			int index = cookieKeyValues.get(i).indexOf(";");
+			if (index < 0) {
+				continue;
+			}
+			String kvString = cookieKeyValues.get(i).substring(0, index);
+			System.out.println(kvString);
+			String[] kvItem = kvString.trim().split("=",2);
+			mapCookies.put(kvItem[0], kvItem[1]);
+		}
+		
+		storeProperties(mapCookies,propertiesFile,append);
+	}
+  
+}
