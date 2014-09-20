c86feeb2da152a0d43ba371fc09e05e41cce6bab
cherish2015 <cherish1896@126.com>
2014-09-20 18:00:24 +0800

    test properties

--- src/test/util/properties/PropertiesUtil.java
+++ src/test/util/properties/PropertiesUtil.java
@@ -1,8 +1,10 @@
 package test.util.properties;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -14,6 +16,39 @@ import test.util.codec.BaseCodecUtil;
 
 
 public class PropertiesUtil {
+	
+
+	public static String getLastDateTime() throws Exception{
+		InputStream input = new FileInputStream("config/config.ini");
+		Properties props = new Properties();
+		props.load(input);
+		String lastDateTime = "";
+		if (props.containsKey("lastDateTime")) {
+			lastDateTime = props.getProperty("lastDateTime");
+		}else{
+			lastDateTime = "0";
+		}
+		return lastDateTime;
+	}
+	
+	
+	public static void setLastDateTime(String lastDateTime) throws Exception{
+		OutputStream out = new FileOutputStream("config/config.ini");
+		Properties props = new Properties();
+		props.put("lastDateTime", lastDateTime);
+		props.store(out, "");
+	}
+	
+	public static void main(String[] args) {
+		try {
+			setLastDateTime("2014-99-22 14:58:35");
+			String lastDateTime = getLastDateTime();
+			System.err.println(lastDateTime);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	
 	public static void storeProperties(Map<String, String> kvmap, String file, boolean append) throws Exception{
 		Map<String, String> kvmapCodec = new HashMap<String, String>();
