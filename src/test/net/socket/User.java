61323837302a61f6d1e27a8086a0ce9e393166b0
cherish2015 <cherish1896@126.com>
2014-09-22 01:19:22 +0800

    test socket object serial

--- src/test/net/socket/User.java
+++ src/test/net/socket/User.java
@@ -3,6 +3,7 @@ package test.net.socket;
 import java.io.Serializable;
 
 public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
 	private String username;
 	private String password;
 	public String getUsername() {
