27586a07d3a89af9defd3468ccfd4404af9b90d3
cherish2015 <cherish1896@126.com>
2014-09-21 23:47:13 +0800

    test socket

--- src/test/net/socket/TestSocket.java
+++ src/test/net/socket/TestSocket.java
@@ -1,13 +1,23 @@
 package test.net.socket;
 
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import test.ITest;
 
 public class TestSocket implements ITest {
+	
+	private static Logger log = LoggerFactory.getLogger(TestSocket.class);
 
+	@Test
 	@Override
 	public void test() {
-		// TODO Auto-generated method stub
-
+		testSocketObject();
+	}
+	
+	public void testSocketObject(){
+		log.info("testSocketObject");
 	}
 
 }
